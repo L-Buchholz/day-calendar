b091c37e9b1b1bec5612a7954eadd1ca871ea6f7
Lauren Buchholz <l.buchholz1988@gmail.com>
2021-11-12 11:43:09 -0700

    Another update

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ var hour = moment().hour();
 console.log(hour);
 
 //Adds past, present, or future classes based on current time
+//Starts at 8, goes until less than 18, add +1 every time the "for loop" loops (which is continous)
 for (var calendarHour = 8; calendarHour < 18; calendarHour++) {
   var currentHour = $("#" + calendarHour);
   if (hour == calendarHour) {
